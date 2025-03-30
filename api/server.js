0d2e4245f1b62a4fcc203de82b452c52577eea7f
KISHORRAGURAM <kishorraguram.g2023cse@sece.ac.in>
2025-03-30 09:56:27 +0530

    inital comit

--- api/server.js
+++ api/server.js
@@ -1,28 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const config = require("./config");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "gkishorraguram@gmail.com", 
-    pass: "pass key", 
+    user: config.emailUser,
+    pass: config.emailPass,
   },
 });
 
-
 app.post("/send-email", async (req, res) => {
   const { recipientEmail, subject, message } = req.body;
-
   const mailOptions = {
-    from: "kishorraguram@gmail.com", 
+    from: config.emailUser,
     to: recipientEmail,
-    subject: subject,
+    subject,
     text: message,
   };
 
@@ -35,8 +34,7 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-
-const PORT = 5000;
+const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
