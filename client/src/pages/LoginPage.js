0f3c5e400e733890270d7f0a2d2190bbcdfa2c4f
KISHORRAGURAM <kishorraguram.g2023cse@sece.ac.in>
2025-03-30 22:47:19 +0530

    second commit

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -11,7 +11,7 @@ export default function LoginPage({setProfilename,setUserId}) {
 async function login(ev) {
     ev.preventDefault();
     try {
-      const response = await fetch("http://localhost:4000/login", {
+      const response = await fetch("https://blogapp-server-mocha.vercel.app/login", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -7,7 +7,7 @@ export default function RegisterPage() {
   const navigate = useNavigate();
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://blogapp-server-mocha.vercel.app/register', {
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
